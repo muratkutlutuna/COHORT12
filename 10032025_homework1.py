7afd39fdb4f92d9f85ac7305dc34ff6b5e7986d0
Murat Kutlu Tuna <muratkutlutuna@murats-macbook-pro.home>
2025-03-12 14:57:01 +0100

    homwork_with_team

--- 10032025_homework1.py
+++ 10032025_homework1.py
@@ -2,21 +2,31 @@
 # Write a short Python program that asks the user to enter 
 # Celcius temperature (it can be a decimal number), 
 # converts the entered temperature into Fahrenheit degree and prints the result
-
+# cels = int(input("Please enter the Temperature in Celsius degree : "))
+# fahr = (cels * 1.8) +32
+# print("This is the Fahrenheit format of the ",cels,"Celsius : ",fahr,"F",sep="")
 
 # Task-2:
 # Write a short Python program that asks the user to enter
 # a distance (it can be a decimal number) in kilometers, 
 # converts the entered distance into miles and prints the results.
 
-print(not 0 and"write me")
+# dist_km = float(input("Please enter the Distance in KM : "))
+# dist_mile = dist_km*0.621371
+# print("This is the mile vesion of the given", dist_km, "distance :",dist_mile)
+
+# print(not 0 and"write me")
 
 year = int(input("Please enter the year?"))
-result = False
-if year%4 == 0: 
-    if year%100 == 0: 
-        if year%400 == 0:
-            result = True
-    else: 
-        result = True
-print(f"Is year {year} leap? : {result}")
+# result = False
+# if year%4 == 0: 
+#     if year%100 == 0: 
+#         if year%400 == 0:
+#             result = True1800
+
+#     else: 
+#         result = True
+# print(f"Is year {year} leap? : {result}")
+# print((year % 100 != 0 and year % 4 == 0) or year % 400 == 0)
+
+
