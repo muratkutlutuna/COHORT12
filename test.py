7403576226494cfe185b9dd08a3f55521570c09d
muratkutlutuna <muratkutlutuna@gmail.com>
2025-03-27 15:40:59 +0100

    sets_and_dictionaries

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print("hello world!")
+set_1 = {'red', 'blur', 'pink', 'red'}
+colors = 'red', 'blur', 'pink', 'red'
+set_2 = set(colors)
+print(set_1, type(set_1))
+print(set_2, type(set_2))   
+print(colors, type(colors))
